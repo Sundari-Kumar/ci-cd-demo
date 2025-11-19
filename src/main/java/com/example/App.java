e11f287065ed09b434851eb923e1d269ec23c462
Sundari K <sundari.k31082004@gmail.com>
2025-11-19 16:12:04 +0530

    updated

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -4,12 +4,12 @@ package com.example;
  * Hello world!
  *
  */
-public class App 
-{
+public class App {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println( "Hello World!" );;
+        System.out.println( "Hello World!" );
+        public
 
     }
 }
